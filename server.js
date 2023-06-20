9afffaaccf08215e18363062faf1fdb0d700758a
gulnabi afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-06-20 18:47:16 +0500

    learn about the ev and dontenv

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+const dotenv = require("dotenv");
+
+// 👉 read the file and save the varialbles to node environment
+dotenv.config({ path: "./config.env" });
+// console.log(process.env);
+
 const app = require("./app");
 
+// 👉 konsa enviroment chal raha hein
+console.log(app.get("env"));
+// 👉 will gave you all the enviromental varialble
+// console.log(process.env);
+
 // port
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`server is running ${port}`);

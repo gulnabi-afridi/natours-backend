99a691034f441330340ed65fb0cb75bb5e0c34a8
gulnabi afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-11-06 23:14:32 +0500

    userModel added

---
+++ models/userModel.js
@@ -0,0 +1,101 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'please tell us your name'],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'please provide your email'],
+    trim: true,
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'please provide a valid email'],
+  },
+  photo: String,
+  role: {
+    type: String,
+    enum: ['user', 'guide', 'lead-guide', 'admin'],
+    default: 'user',
+  },
+  password: {
+    type: String,
+    required: [true, 'A user must gave a password'],
+    minlength: 8,
+    select: false,
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, 'please conform your password'],
+    // THIS ONLY WORKS ON CREATING NEW USER NOT FOR UPDATING THE USER!!!
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'passwords are not the same',
+    },
+  },
+  passwordChangedAt: {
+    type: Date,
+  },
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+});
+
+// DOCUMENT PRE MIDLEWARE
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  // 12 mean cost the higher this cost basically the more cpu intensive the process will be
+  this.password = await bcrypt.hash(this.password, 12);
+  this.confirmPassword = undefined;
+  next();
+});
+
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password') || this.isNew) return next();
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+//  This method would be available on all the user document everywhere.
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword, // comming from user
+  userPassword, // point to the hash password
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000);
+    // console.log(changedTimestamp, JWTTimestamp);
+    return JWTTimestamp < changedTimestamp;
+  }
+  // false mean password not changed
+  return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+  // encrypting that token
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  // console.log({ resetToken }, this.passwordResetToken);
+
+  // we need in milliseconds
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+  return resetToken;
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;

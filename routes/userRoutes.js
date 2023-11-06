88e147f7070f4bf989f77cd2deafb4b6cb6837e1
gulnabi afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-11-06 23:13:04 +0500

    modified userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,24 @@
-const express = require("express");
+const express = require('express');
 const {
   getAllUser,
   creatNewUser,
   getUser,
   updateUser,
   deleteUser,
-} = require("../controllers/userController");
+} = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.route("/").get(getAllUser).post(creatNewUser);
-router.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
+// sign Up route
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+router.route('/').get(getAllUser).post(creatNewUser);
+router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 module.exports = router;
